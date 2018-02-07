fdf501f25b39a3d4e1cb0adcfc2c68813283bcc1
Anders Weijnitz <aweijnitz@sapient.com>
2018-02-07 13:56:06 +0100

    start of message handling

--- routes/messageHandler.js
+++ routes/messageHandler.js
@@ -11,6 +11,20 @@ const fs = require('fs');
  */
 const isDefined = function (str) {
     return (typeof str != 'undefined' && null != str && '' != str);
+};
+
+function firstEntity(nlp, name) {
+    return nlp && nlp.entities && nlp.entities[name] && nlp.entities[name][0];
+}
+
+function handleMessage(message) {
+    // check greeting is here and is confident
+    const greeting = firstEntity(message.nlp, 'greetings');
+    if (greeting && greeting.confidence > 0.8) {
+        return 'Hi there!';
+    } else {
+        return 'Default reply :-)';
+    }
 }
 
 
@@ -25,7 +39,7 @@ const handleReq = function (appConf, log4js) {
     return function webhook(req, res) {
         let body = req.body;
 
-	//logger.info(util.inspect(req.body));
+        //logger.info(util.inspect(req.body));
 
         // Checks this is an event from a page subscription
         if (body.object === 'page') {
@@ -33,14 +47,24 @@ const handleReq = function (appConf, log4js) {
             // Iterates over each entry - there may be multiple if batched
             body.entry.forEach(function (entry) {
 
-                // Gets the message. entry.messaging is an array, but
-                // will only ever contain one message, so we get index 0
-                let webhook_event = entry.messaging[0];
-                logger.debug(webhook_event);
+                    // Gets the message. entry.messaging is an array, but
+                    // will only ever contain one message, so we get index 0
+                    let webhook_event = entry.messaging[0];
+                    logger.debug(webhook_event);
 
-		let nlp = webhook_event.message.nlp;
-		logger.debug(util.inspect(nlp, { showHidden: false, depth: 5, colors: true }));
-            });
+                    if (!!webhook_event.message) {
+
+                        if (!!webhook_event.message.nlp) {
+                            let nlp = webhook_event.message.nlp;
+                            logger.debug(util.inspect(nlp, {showHidden: false, depth: 5, colors: true}));
+                        }
+
+                        let reply = handleMessage(webhook_event.message);
+                        logger.debug('REPLY - ' + reply);
+                        res.status(200).send(reply);
+                    }
+                }
+            );
 
             // Returns a '200 OK' response to all requests
             res.status(200).send('EVENT_RECEIVED');

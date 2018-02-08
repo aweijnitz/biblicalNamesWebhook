d359f7e341f67f7800175f07eb15c961f4f1de4f
Anders Weijnitz <aweijnitz@sapient.com>
2018-02-08 13:32:08 +0100

    incremental fix

--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,7 @@ const messageHandler = require('./messageHandler');
 // Remember, in Express 4, '/' is the root under which this route is mounted, so does not
 // necessarily correspond to the absolute root of the domain.
 //
-router.get('/biblicalnamesbot', function(req, res) {
+router.get('/', function(req, res) {
   logger.debug('Serving / --> index.hjs');
   res.render('index', { title: 'Biblical Names Webhook' });
 });

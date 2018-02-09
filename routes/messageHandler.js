3812f1db24ad6344df5d06d6d8978dd2a9470ab6
anders <andersw@noone.it>
2018-02-09 21:56:31 +0100

    cleanup

--- routes/messageHandler.js
+++ routes/messageHandler.js
@@ -1,29 +1,11 @@
-//const appConf = require('../conf/appConfig.json');
-const os = require('os');
-const util = require('util');
-const fs = require('fs');
-
 const {Wit, log} = require('node-wit');
-
-
-
-/**
- * Private helper to sanity check a string before using it.
- * @param str
- * @returns {boolean}
- */
-const isDefined = function (str) {
-    return (typeof str != 'undefined' && null != str && '' != str);
-};
 
 module.exports = function messageHandler(message, sender) {
 
     const client = new Wit({
-        accessToken: 'DEK5LYCA2JTYEX43EBVJSNKG5JNPX6X4',
+        accessToken: process.env.WIT_AI_APP_TOKEN,
         logger: new log.Logger(log.DEBUG) // optional
     });
 
-
     return client.message(message.text, {});
-
 };
